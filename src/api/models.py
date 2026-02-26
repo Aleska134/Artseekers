8d1ca2b981e40d55f14264201f77e134c539064f
Aleska P Braschi <143849441+Aleska134@users.noreply.github.com>
2026-02-26 04:59:51 +0000

    Fix: increase password column length for hashes

--- src/api/models.py
+++ src/api/models.py
@@ -19,7 +19,7 @@ class User(db.Model):
     name = db.Column(db.String(120), unique=False, nullable=False)
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(250), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     profile_image = db.Column(db.String(255), unique=False, nullable=True) 
 
